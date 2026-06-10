4e0c7daec6accd7ff3f848249e30e64ffeca637c
Abhimael <abhima_si@outlook.com>
2026-06-10 18:35:57 -0300

    feat(ClienteDAO): update class methods
    -Uncomment getSqlInserir and getSqlAtualizar methods
    -add retornarSelecionado method: returns the resultSet selected Cliente

--- JavaPet/src/main/java/com/mycompany/javapet/controller/ClienteDAO.java
+++ JavaPet/src/main/java/com/mycompany/javapet/controller/ClienteDAO.java
@@ -1,6 +1,7 @@
 package com.mycompany.javapet.controller;
 
 import com.mycompany.javapet.model.Cliente;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ClienteDAO extends GenericDAO<Cliente> {
@@ -14,15 +15,15 @@ public class ClienteDAO extends GenericDAO<Cliente> {
         return "cliente";
     }
 
-//    @Override
-//    public String getSqlInserir() {
-//        return  "INSERT INTO "+getNomeTabela()+" (nome, telefone, endereco) VALUES (?, ?, ?)";
-//    }
-//
-//    @Override
-//    public String getSqlAtualizar() {
-//        return "UPDATE "+getNomeTabela()+" SET nome = ?, telefone = ?, endereco = ? WHERE id = ?";
-//    }
+    @Override
+    public String getSqlInserir() {
+        return  "INSERT INTO "+getNomeTabela()+" (nome, telefone, endereco) VALUES (?, ?, ?)";
+    }
+
+    @Override
+    public String getSqlAtualizar() {
+        return "UPDATE "+getNomeTabela()+" SET nome = ?, telefone = ?, endereco = ? WHERE id = ?";
+    }
 
     @Override
     public ArrayList<Cliente> retornarLista() {
@@ -31,7 +32,20 @@ public class ClienteDAO extends GenericDAO<Cliente> {
 
     @Override
     public Cliente retornarSelecionado() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try{
+            Cliente cliente = null;
+            if(resultSet != null){
+                cliente = new Cliente();
+                cliente.setId(String.valueOf(resultSet.getInt("id")));
+                cliente.setNome(resultSet.getString("nome"));
+                cliente.setTelefone(resultSet.getInt("telefone"));
+                cliente.setEndereco(resultSet.getString("endereco"));
+            }
+            return cliente;
+        }
+        catch(SQLException err){
+            System.out.println("Erro ao acessar statement: "+err.getMessage());
+        }
+        return null;
     }
-    
 }
